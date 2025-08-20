c71533055a7b8570b4b96753e2c09dd6559f80f9
Dema Therese Maria Palathingal <dema@mac.lan>
2025-08-20 09:50:21 -0500

    doc: update device_id descripts #67(https://github.com/Common-Longitudinal-ICU-data-Format/CLIF/issues/67)

--- src/content/v-2-1-0-ddl.sql
+++ src/content/v-2-1-0-ddl.sql
@@ -30,7 +30,7 @@ CREATE TABLE code_status(
 -- -----------------------------------------------------
 CREATE TABLE crrt_therapy (
   hospitalization_id VARCHAR COMMENT '{"description": "Unique identifier for hospitalization episode", "permissible": "No restriction"}',
-  device_id VARCHAR COMMENT '{"description": "Unique identifier for dialysis machine", "permissible": "No restriction"}',
+  device_id VARCHAR COMMENT '{"description": "Unique ID of the individual dialysis machine used (e.g., machine ACZ3RV91). Distinct from dialysis_machine_name, which stores the brand/model.", "permissible": "No restriction"}',
   recorded_dttm DATETIME COMMENT '{"description": "Timestamp when CRRT parameters were recorded", "permissible": "Datetime format should be YYYY-MM-DD HH:MM:SS+00:00 (UTC)"}',
   crrt_mode_name VARCHAR COMMENT '{"description": "Name of CRRT mode (e.g., CVVHDF)", "permissible": "No restriction"}',
   crrt_mode_category VARCHAR COMMENT '{"description": "Standardized CRRT mode categories", "permissible": "[scuf, cvvh, cvvhd, cvvhdf](https://github.com/Common-Longitudinal-ICU-data-Format/CLIF/blob/main/mCIDE/crrt_therapy/clif_crrt_therapy_mode_categories.csv)"}',
@@ -216,7 +216,7 @@ CREATE TABLE respiratory_support (
   hospitalization_id VARCHAR COMMENT '{"description": "ID variable for each patient encounter", "permissible": "No restriction"}',
   recorded_dttm DATETIME COMMENT '{"description": "Date and time when the device settings and/or measurement was recorded. All datetime variables must be timezone-aware and set to UTC.", "permissible": "Datetime format should be YYYY-MM-DD HH:MM:SS+00:00"}',
   device_name VARCHAR COMMENT '{"description": "Raw string of the device. Not used for analysis.", "permissible": "No restriction"}',
-  device_id VARCHAR COMMENT '{"description": "ID variable for device used", "permissible": "No restriction"}',
+  device_id VARCHAR COMMENT '{"description": "Unique ID of the individual physical device used (e.g., ventilator ACZ3RV91); enables linkage to continuous waveform data. Distinct from vent_brand_name, which stores the brand/model.", "permissible": "No restriction"}',
   device_category VARCHAR COMMENT '{"description": "Maps device_name to a standardized list of respiratory support device categories", "permissible": "[\"IMV\", \"NIPPV\", \"CPAP\", \"High Flow NC\", \"Face Mask\", \"Trach Collar\", \"Nasal Cannula\", \"Room Air\", \"Other\"](https://github.com/Common-Longitudinal-ICU-data-Format/CLIF/blob/main/mCIDE/respiratory_support/clif_respiratory_support_device_categories.csv)"}',
   vent_brand_name VARCHAR COMMENT '{"description": "Ventilator model name when device_category is IMV or NIPPV", "permissible": "Optional"}',
   mode_name VARCHAR COMMENT '{"description": "Raw string of ventilation mode (e.g., CMV volume control)", "permissible": "No restriction"}',
